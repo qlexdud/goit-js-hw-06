7e407145d5cc4aad5102de2a504b38367b71b163
qlexdud <133101510+qlexdud@users.noreply.github.com>
2023-10-03 23:14:09 +0300

    початок_закладено

---
+++ src/js/task-01.js
@@ -0,0 +1,7 @@
+const numberCategories = categories.children.length;
+console.log(`Number of categories: ${numberCategories}`)
+const lister = document.body.categories;
+[...document.getElementsByClassName("categories")].forEach((category) => {
+    //const firstListCategory = category.firstElementChild;
+    console.log(category);
+});
